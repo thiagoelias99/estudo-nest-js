aea9b965b6903264675db4935b336b10fae39b2c
Thiago Elias <thiagoelias99@gmail.com>
2023-11-15 16:59:50 -0300

    feat: added postgres database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,22 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { UsersModule } from './users/users.module'
 import { ProductsModule } from './products/products.module'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { DbConfigService } from './config/db.config.service'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-    imports: [UsersModule, ProductsModule],
+    imports: [
+        UsersModule, 
+        ProductsModule,
+        TypeOrmModule.forRootAsync({
+            useClass: DbConfigService,
+            inject: [DbConfigService]
+        }),
+        ConfigModule.forRoot({
+            isGlobal: true
+        })
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
